627bc4b5097480531211f99123a8eff30e333aa9
CVIOLLET <cecile.viollet@espci.fr>
2024-08-30 16:54:01 +0200

    coucou

---
+++ tests/test_cecile.py
@@ -0,0 +1,28 @@
+import unittest
+from pathlib import Path
+
+from common_log import basic_config_log
+from configuration import Configuration
+from model import Experiment
+
+
+class TestCecile(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        basic_config_log()
+
+    def test_load_experiment(self):
+        config = Configuration(base_dir=Path('/Users/macminicv/Documents/Data_SC/CorrectedLocalSniffer'))
+        xp = Experiment.load(xp_name="XP9", delete_cache=True)
+
+    # def test_MiceOccupation(self):
+    #     config = Configuration(base_dir=Path('./resources'))
+    #
+    #     print("ok")
+    #     # self.assertEqual(Path('./resources'), config.get_base_dir())
+    #     #
+    #     # config2 = Configuration()
+    #     #
+    #     # self.assertIs(config, config2)
+
